24e9970df1c9c1405a3e9b555e933a478fde76ae
Abid Mahmood <abidmahmoodbutt@hotmail.com>
2015-11-16 16:26:00 +0500

    Stabilising server of node.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+'use strict';
+require("babel/register");
+require("./server.js");
+
+import express from "express";
+
+let app = express();
+let PORT = 3000;
+
+app.post('/graphql', (req, res) => {
+  res.send("Hello");
+})
+
+const server = app.listen(PORT, () => {
+  let host = server.address().address;
+  let port server.address().port;
+
+  console.log("GraphQL server is listening on %s:%s", host, port);
+})
